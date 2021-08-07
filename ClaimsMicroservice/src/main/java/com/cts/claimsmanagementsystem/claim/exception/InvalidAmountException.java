2fbd0b672f12143e1d843ab261c9d084fa1a7758
Vani Shinghal <vanishinghal1@gmail.com>
2021-08-07 18:17:28 +0530

    Added Amount Exception

---
+++ ClaimsMicroservice/src/main/java/com/cts/claimsmanagementsystem/claim/exception/InvalidAmountException.java
@@ -0,0 +1,15 @@
+package com.cts.claimsmanagementsystem.claim.exception;
+
+public class InvalidAmountException extends Exception{
+	
+	public InvalidAmountException(String msg) {
+		super(msg);
+		
+	}
+	
+	public InvalidAmountException() {
+		super();
+		
+	}
+	
+}
