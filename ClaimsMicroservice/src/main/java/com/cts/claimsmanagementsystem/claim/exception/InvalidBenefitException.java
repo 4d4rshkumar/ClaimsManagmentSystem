f1289881c3b9af2a2cbf1509f3c3fc22c107518f
Vani Shinghal <vanishinghal1@gmail.com>
2021-08-07 18:00:53 +0530

    Added Exceptions.

---
+++ ClaimsMicroservice/src/main/java/com/cts/claimsmanagementsystem/claim/exception/InvalidBenefitException.java
@@ -0,0 +1,18 @@
+package com.cts.claimsmanagementsystem.claim.exception;
+
+public class InvalidBenefitException extends Exception{
+
+	
+	public InvalidBenefitException(String msg) {
+		super(msg);
+		
+	}
+	
+	public InvalidBenefitException() {
+		super();
+		
+	}
+	
+	
+	
+}
---
+++ ClaimsMicroservice/src/main/java/com/cts/claimsmanagementsystem/claim/exception/InvalidProviderException.java
@@ -0,0 +1,14 @@
+package com.cts.claimsmanagementsystem.claim.exception;
+
+public class InvalidProviderException extends Exception{
+
+		
+	public InvalidProviderException(String msg) {
+
+		super(msg);
+	}
+	public InvalidProviderException() {
+
+		super();
+	}
+}
